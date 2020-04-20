580b97ecab60b774f02acb6db0db885e85445c49
Anthony Flores <aflores9192@gmail.com>
2020-04-20 04:16:36 -0500

    ChirpItem - bare bones - complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import ChirpItem from "./molecules/A_Shared/ChirpItem"
 
 function App() {
-  return <body></body>;
+  return (
+    <div className="App">
+      {/* <div style={{ display: "flex", width: "100%" }}>
+        <ChirpItem />
+      </div> */}
+    </div>
+  )
 }
 
 export default App;
