ef7db276e20b27ed4bc4e3a66138a501f4af7b83
Anthony Flores <aflores9192@gmail.com>
2020-06-01 20:05:10 -0500

    Finished Like button functionality

--- src/components/molecules/A_Shared/ChirpItem.js
+++ src/components/molecules/A_Shared/ChirpItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./a_styles/ChirpItem.css";
 import LikeIcon from "../../atoms/ChripItem/LikeIcon";
 const profilePic =
@@ -16,8 +16,50 @@ const likeIcon =
 const likeNum = 6;
 const contextIcon =
   "https://res.cloudinary.com/techienaut/image/upload/v1590375851/Chirper/ChripItem/Context_dqou7q.png";
+let defaultLikeNum = function (propsLikeNum) {
+  if (propsLikeNum === undefined) {
+    return 0;
+  } else {
+    return propsLikeNum;
+  }
+};
+// let reducerLikeNum = function (state, action) {
+//   return state;
+// };
 function ChirpItem(props) {
+  // const [likeNum, dispatchLikeNum] = useReducer(
+  //   reducerLikeNum,
+  //   defaultLikeNum(props.likeNum)
+  // );
+  // const [incrementLikeNum, setIncrementLikeNum] = useState(true);
+  const [likeNum, setLikeNum] = useState(defaultLikeNum(props.likeNum));
   const [isHoverLike, setHoverLike] = useState(false);
+  const [likeNumColor, setLikeNumColor] = useState("var(--lynch)");
+  const [isClickLike, setClickLike] = useState(false);
+  const [isIncrementLike, setIncrementLike] = useState(true);
+  useEffect(() => {
+    if (isHoverLike) {
+      setLikeNumColor("var(--cerise)");
+    } else if (isClickLike) {
+      setLikeNumColor("var(--cerise)");
+    } else {
+      setLikeNumColor("var(--lynch)");
+    }
+  }, [isHoverLike]);
+  useEffect(() => {
+    if (isClickLike) {
+      setLikeNumColor("var(--cerise)");
+    } else if (isHoverLike) {
+      setLikeNumColor("var(--cerise)");
+    } else {
+      setLikeNumColor("var(--lynch)");
+    }
+  }, [isClickLike]);
+  let handleClickLike = function () {
+    setClickLike(!isClickLike);
+    setLikeNum(isIncrementLike ? likeNum + 1 : likeNum - 1);
+    setIncrementLike(!isIncrementLike);
+  };
   return (
     <div className="chirp-item">
       <div className="icon-div">
@@ -39,14 +81,18 @@ function ChirpItem(props) {
         <LikeIcon
           onMouseEnter={() => setHoverLike(true)}
           onMouseLeave={() => setHoverLike(false)}
+          onClick={handleClickLike}
           hover={isHoverLike}
+          clicked={isClickLike}
         />
         <span
           className="like-num color-lynch"
+          style={{ color: likeNumColor }}
           onMouseEnter={() => setHoverLike(true)}
           onMouseLeave={() => setHoverLike(false)}
+          onClick={handleClickLike}
         >
-          {props.likeNum}
+          {likeNum}
         </span>
       </div>
       <div className="context-div">
